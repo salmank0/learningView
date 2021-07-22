2ac40d85f3a5591707d48a0671cb962652d1297b
salmank0 <mohammadsalmank00@gmail.com>
2021-07-22 14:16:36 +0530

    Added index file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const name= "Salman"
+const vueApp = Vue.createApp({
+    data(){
+        return{
+            name : "Salman Khan",
+            description : "This is very good vue app",
+            url : "https://www.youtube.com/"
+        }
+    }
+})
